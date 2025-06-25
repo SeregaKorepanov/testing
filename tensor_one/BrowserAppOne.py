ad8dd2916d531fa16bc5531529a0cfec9e0719e5
Сергей Корепанов <veselchakglu@gmail.com>
2025-06-25 11:25:40 +0300

    Первое задание

---
+++ tensor_one/BrowserAppOne.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from LocatorsOne import Locator
+
+class BrowserToolsInit:
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.maximize_window()
+        self.base_url = Locator.base_url
+
+
+    def find_element(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(
+            EC.presence_of_element_located(locator)
+        )
+
+    def find_elements(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(
+            EC.presence_of_all_elements_located(locator)
+        )
+
+    def start_site(self):
+        return self.driver.get(self.base_url)
