43ecf2394dfbb20a642e75d174f5e328ef4f11b8
Серёжа Корепанов <90341619+SeregaKorepanov@users.noreply.github.com>
2025-06-25 10:06:29 +0300

    Delete conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture(scope="session")
-def browser():
-    chrome_driver = webdriver.Chrome()
-    yield chrome_driver
-    chrome_driver.quit()
